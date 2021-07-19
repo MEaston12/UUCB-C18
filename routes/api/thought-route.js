b875e0dd402562ea512f6fcbedb2ec3e19557794
Malcolm Easton <eastonmalcolm@gmail.com>
2021-07-18 23:51:20 -0600

    started routes

---
+++ routes/api/thought-route.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { Thought } = require('../../models');
+
+router.get('/', async (req, res) => {
+
+});
+
+router.post('/', async (req, res) => {
+    
+});
+
+router.put('/', async (req, res) => {
+    
+});
+
+router.delete('/', async (req, res) => {
+    
+});
+
+router.post('/:thoughtId/reactions', async (req, res) => {
+    
+});
+
+router.delete('/:thoughtId/reactions', async (req, res) => {
+    
+});
+
+module.exports = router;
---
+++ routes/api/user-route.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.get('/', async (req, res) => {
+
+});;
+
+router.post('/', async (req, res) => {
+    
+});
+
+router.put('/', async (req, res) => {
+    
+});
+
+router.delete('/', async (req, res) => {
+    
+});
+
+router.post('/:userId/friends/:friendId', async (req, res) => {
+    
+});
+
+router.delete('/:userId/friends/:friendId', async (req, res) => {
+    
+});
+
+module.exports = router;

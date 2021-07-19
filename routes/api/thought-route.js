80e4d9ca4e84302093f88768b1f053fc7d84fa6c
Malcolm Easton <eastonmalcolm@gmail.com>
2021-07-19 02:18:14 -0600

    populated routes

--- routes/api/thought-route.js
+++ routes/api/thought-route.js
@@ -1,28 +1,54 @@
 const router = require('express').Router();
-const { Thought } = require('../../models');
+const { Thought, User } = require('../../models');
 
 router.get('/', async (req, res) => {
+    const result = await Thought.find({}).catch(err => err);
+    res.json(result);
+});
 
+router.get('/:id', async (req, res) => {
+    const result = await Thought.findOne({ _id: req.params.id }).catch(err => err);
+    if(!result) res.json({message: 'No thought found!'});
+    res.json(result);
 });
 
 router.post('/', async (req, res) => {
-    
+    try {
+        const result = await Thought.create(req.body);
+        await User.findOneAndUpdate({ username: req.body.username }, { $push: { thoughts: result._id }});
+        res.json(result);
+    } catch (err) {
+        res.json(err);
+    }
 });
 
-router.put('/', async (req, res) => {
-    
+router.put('/:id', async (req, res) => {
+    const result = await Thought.findOneAndUpdate({ _id: req.params.id }, req.body, {new: true}).catch(err => err);
+    if(!result) res.json({message: 'No thought found!'});
+    res.json(result);
 });
 
-router.delete('/', async (req, res) => {
-    
+router.delete('/:id', async (req, res) => {
+    try {
+        const result = await Thought.findOneAndDelete({ _id: req.params.id });
+        if(!result) return res.json({message: 'No thought found!'});
+        await User.findOneAndUpdate({ username: result.username }, { $pull: { thoughts: result._id }});
+        res.json(result);
+    } catch (err) {
+        res.json(err);
+    }
 });
 
 router.post('/:thoughtId/reactions', async (req, res) => {
-    
+    const result = await Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $push: { reactions: req.body }}, {new: true}).catch(err => err);
+    if(!result) res.json({message: 'No thought found!'});
+    res.json(result);
 });
 
-router.delete('/:thoughtId/reactions', async (req, res) => {
-    
+router.delete('/:thoughtId/reactions/:reactionId', async (req, res) => {
+    const result = await Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $pull: { reactions: { reactionId: req.params.reactionId }}}, {new: true}).catch(err => err);
+    if(!result) res.json({message: 'No thought found!'});
+    res.json(result);
 });
 
 module.exports = router;
--- routes/api/user-route.js
+++ routes/api/user-route.js
@@ -2,27 +2,43 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 router.get('/', async (req, res) => {
+    const result = await User.find({}).catch(err => err);
+    res.json(result);
+});
 
-});;
+router.get('/:id', async (req, res) => {
+    const result = await User.findOne({ _id: req.params.id }).catch(err => err);
+    if(!result) res.json({message: 'No user found!'});
+    res.json(result);
+});
 
 router.post('/', async (req, res) => {
-    
+    const result = await User.create(req.body).catch(err => err);
+    res.json(result);
 });
 
-router.put('/', async (req, res) => {
-    
+router.put('/:id', async (req, res) => {
+    const result = await User.findOneAndUpdate({ _id: req.params.id }, req.body, {new: true}).catch(err => err);
+    if(!result) res.json({message: 'No user found!'});
+    res.json(result);
 });
 
-router.delete('/', async (req, res) => {
-    
+router.delete('/:id', async (req, res) => {
+    const result = await User.findOneAndDelete({ _id: req.params.id }).catch(err => err);
+    if(!result) res.json({message: 'No user found!'});
+    res.json(result);
 });
 
 router.post('/:userId/friends/:friendId', async (req, res) => {
-    
+    const result = await User.findOneAndUpdate({ _id: req.params.userId }, { $addToSet: { friends: req.params.friendId }}, {new: true}).catch(err => err);
+    if(!result) res.json({message: 'No user found!'});
+    res.json(result);
 });
 
 router.delete('/:userId/friends/:friendId', async (req, res) => {
-    
+    const result = await User.findOneAndUpdate({ _id: req.params.userId }, { $pull: { friends: req.params.friendId }}, {new: true}).catch(err => err);
+    if(!result) res.json({message: 'No user found!'});
+    res.json(result);
 });
 
 module.exports = router;
